e8013ad05547f09718ca1506d97c56da6dc5be02
loretari <loretari@gmail.com>
2020-12-07 13:45:50 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -11,9 +12,11 @@ class App extends Component {
 
   render() {
     return (
+    	<BrowserRouter>
      <div>
         <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
